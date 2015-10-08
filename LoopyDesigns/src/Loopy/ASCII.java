540405ecb3683aaee653f2811e962cc8658c2679
kbowen99 <kurtisbowen99@gmail.com>
2015-10-08 10:14:29 -0500

    Actually Making it work...

--- LoopyDesigns/src/Loopy/ASCII.java
+++ LoopyDesigns/src/Loopy/ASCII.java
@@ -3,8 +3,58 @@ package Loopy;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
+
 public class ASCII {
-	public void DoASCII(){
+	
+	//Image Loading/Interpreting Code based on AJ Engine V1.0 (Written by AJ)
+	
+	
+	public String path;
+	public int width, height;
+	public int[] pixels;
+	
+	public void DoASCII(String path){
 		
+		BufferedImage image = null;
+
+		// try to load image (not found throws an exception).
+		try {
+			image = ImageIO.read(ASCII.class.getResourceAsStream(path));
+			System.out.println("Asset Loaded");
+		} catch (final Exception e) {
+			e.printStackTrace();
+			System.out.println("Asset Failed");
+		}
+
+		if (image == null) {
+			return;
+		}
+
+		// sets up path and vars.
+		this.path = path;
+		width = image.getWidth();
+		height = image.getHeight();
+
+		// makes the RGB image contain an RGB image
+		pixels = image.getRGB(0, 0, width, height, null, 0, width);
+
+		// inserts a 0, 1, 2 or 3 into the spritesheet
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = (pixels[i] & 0xff) / 64;
+		}
+		for (int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
+				if (pixels[(i * width) + j] > 2) {
+					System.out.print("   ");
+				} else if (pixels[(i * width) + j] > 1) {
+					System.out.print(" ' ");
+				} else if (pixels[(i * width) + j] > 0) {
+					System.out.print(" - ");
+				} else {
+					System.out.print(" = ");
+				}
+			}
+			System.out.println("");
+		}
 	}
 }
--- LoopyDesigns/src/Loopy/Main.java
+++ LoopyDesigns/src/Loopy/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		ASCII PicDrawer = new ASCII();
+		PicDrawer.DoASCII("/pic.png");
 		//Pretend This is code
 	}
 }
